f54c56fce1b28eab66b46c96bf89c0bf4c0ba00b
sadie21393 <sadie21@byu.edu>
2024-12-03 14:24:38 -0700

    database connection

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'turtle-secret', resave: false, saveUninitialized: true }));
 
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME || "localhost",
+        user: process.env.RDS_USERNAME || "postgres",
+        password: process.env.RDS_PASSWORD || "goEFYbyu#1",
+        database: process.env.RDS_DB_NAME || "turtle",
+        port: process.env.RDS_PORT || 5432
+    }
+});
+
 // Routes
 app.use('/', routes);
 
