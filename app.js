7b71aa36dce0a48abec44189c9bf5775883ba4fe
sadie21393 <sadie21@byu.edu>
2024-12-03 14:40:26 -0700

    ssl connect

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const knex = require("knex")({
         user: process.env.RDS_USERNAME || "postgres",
         password: process.env.RDS_PASSWORD || "goEFYbyu#1",
         database: process.env.RDS_DB_NAME || "turtle",
-        port: process.env.RDS_PORT || 5432
+        port: process.env.RDS_PORT || 5432,
+        ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
 });
 
