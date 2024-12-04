09f349b92196a06dd45881840c5ab158a990857a
sadie21393 <sadie21@byu.edu>
2024-12-04 15:03:50 -0700

    database update

--- models/database.js
+++ models/database.js
@@ -5,19 +5,14 @@ const knex = require("knex")({
     connection: {
         host: process.env.RDS_HOSTNAME,
         user: process.env.RDS_USERNAME,
-        password: process.env.RDS_PASSWORD || '12345678',
+        password: process.env.RDS_PASSWORD,
         database: process.env.RDS_DB_NAME,
         port: process.env.RDS_PORT || 5432,
-        ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
     }
 });
-
-// Test database connection
-knex.raw('SELECT 1')
-    .then(() => console.log('Database connected successfully!'))
-    .catch(err => {
-        console.error('Database connection failed:', err);
-        process.exit(1); // Exit the process if DB connection fails
-    });
 
 module.exports = knex;
