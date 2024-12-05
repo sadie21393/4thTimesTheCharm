35de09502c7a7dfe001c185c9100ef31e3c703d1
sadie21393 <sadie21@byu.edu>
2024-12-05 10:23:36 -0700

    conf nginx

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const knex = require('../models/database');
-const jwt = require('jsonwebtoken');
+
 
 // Landing page
 router.get('/', (req, res) => res.render('index')); //This one worked v8
